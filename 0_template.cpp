dfb0d6d821b37afcd5fe9d5697411f64779fe977
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-10-28 20:17:59 +0600

    28-10-2024

--- 0_template.cpp
+++ 0_template.cpp
@@ -5,31 +5,8 @@
 using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
-//----------------------------(definition section)-------------------------------------
-#define f(i, a, b) for (ll i = a; i < b; i++)
-#define scv(v, n) f(i, 0, n) cin >> (v[i]);
-#define dbg(x) cout << #x << " = " << x << '\n'
-#define nl cout << ("\n")
-#define rrr return
-#define fi first
-#define sc second
-#define pb(x) push_back(x)
-#define ppb(x, y) push_back({x, y})
-
-#define all(s) s.begin(), s.end()
-#define rall(s) s.rbegin(), s.rend()
-
-#define PI acos(-1)
-#define base1 1000002089
-#define base2 1000003853
-#define hashmod 1000002989
-
-#define eps 0.0000000001
-#define inf 90000000000000
-#define MOD 1000000007
-#define N 200009
 //------------------------------------------------------------------------------------
-//-------------------------Modular-Arithmatic-----------------------------------------
+/*/-------------------------Modular-Arithmatic----------------------------------------
 inline ll _normal(ll A, ll M)
 {
     A = A - (A / M) * M;
@@ -70,6 +47,29 @@ inline ll moddiv(ll A, ll B, ll M)
     return _normal(modmul(A, (binexpo(B, M - 2, M)), M), M);
 }
 //----------------------------------------------------------------------------------*/
+//----------------------------(definition section)-------------------------------------
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define scv(v, n) f(i, 0, n) cin >> (v[i]);
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define nl cout << ("\n")
+#define rrr return
+#define fi first
+#define sc second
+#define pb(x) push_back(x)
+#define ppb(x, y) push_back({x, y})
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+#define PI acos(-1)
+#define base1 1000002089
+#define base2 1000003853
+#define hashmod 1000002989
+
+#define eps 0.0000000001
+#define inf 90000000000000
+#define MOD 1000000007
+#define N 200009
 //------------------------------------------------------------------------------------
 void solve(void)
 {
