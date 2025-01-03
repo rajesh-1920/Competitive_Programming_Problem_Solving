cf7f6a610d834e3f157190cb6151868d9b0af841
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2025-01-03 22:57:31 +0600

    03-01-2025

---
+++ Codechef/Wordle.cpp
@@ -0,0 +1,48 @@
+// Author:  Rajesh Biswas
+// CF    :  rajesh-1920
+// Date  :  03.01.2025
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+//----------------------------(definition section)-----------------------------------------
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define fi first
+#define sc second
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const ll inf = 9e15 + 7;
+const ll MOD = 1e9 + 7;
+const ll N = 1e9 + 10;
+//-----------------------------------------------------------------------------------------
+void solve(void)
+{
+    string a, b;
+    cin >> a >> b;
+    for (ll i = 0; i < a.size(); i++)
+    {
+        if (a[i] == b[i])
+            cout << 'G';
+        else
+            cout << 'B';
+    }
+    cout << '\n';
+}
+//-----------------------------------------------------------------------------------------
+int main()
+{
+    // cout << fixed << showpoint << setprecision(10);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1, T;
+    cin >> test;
+    for (T = 1; T <= test; T++)
+    {
+        // cout << "Case " << T << ": ";
+        solve();
+    }
+    return 0;
+}
