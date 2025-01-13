bdf8f949e2f0a5ae178a42d55f79e9ab922d21b4
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2025-01-13 21:46:14 +0600

    13-01-2025

--- Codeforces/C_The_Trail.cpp
+++ Codeforces/C_The_Trail.cpp
@@ -20,9 +20,50 @@ const ll N = 1e5 + 10;
 //-----------------------------------------------------------------------------------------
 void solve(void)
 {
-    ll n;
-    cin >> n;
-    // vector<ll> v(n);
+    ll n, m;
+    string s;
+    cin >> n >> m >> s;
+    ll a[n][m], row[n], col[m];
+    for (ll i = 0; i < n; i++)
+    {
+        row[i] = 0;
+        for (ll j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+            row[i] += a[i][j];
+        }
+    }
+    for (ll i = 0; i < m; i++)
+    {
+        col[i] = 0;
+        for (ll j = 0; j < n; j++)
+            col[i] += a[j][i];
+    }
+    ll i = 0, j = 0;
+    for (ll p = 0; p < s.size(); p++)
+    {
+        if (s[p] == 'D')
+        {
+            a[i][j] = -row[i];
+            col[j] -= row[i];
+            row[i] = 0;
+            i++;
+        }
+        else
+        {
+            a[i][j] = -col[j];
+            row[i] -= col[j];
+            col[j] = 0;
+            j++;
+        }
+    }
+    a[n - 1][m - 1] = -col[m - 1];
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll j = 0; j < m; j++)
+            cout << a[i][j] << ' ';
+        cout << '\n';
+    }
 }
 //-----------------------------------------------------------------------------------------
 int main()
@@ -31,7 +72,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int test = 1, T;
-    // cin >> test;
+    cin >> test;
     for (T = 1; T <= test; T++)
     {
         // cout << "Case " << T << ": ";
