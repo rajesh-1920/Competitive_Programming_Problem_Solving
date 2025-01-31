aab8a29a44b6b9bbd4323ddc31f47908332774d8
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2025-01-31 16:46:36 +0600

    31-01-2025

--- Vjudge/Q_Babelfish.cpp
+++ Vjudge/Q_Babelfish.cpp
@@ -1,19 +1,70 @@
+// Author:  Rajesh Biswas
+// CF    :  rajesh-1920
+// Date  :  31.01.2025
+
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
+//----------------------------(definition section)-----------------------------------------
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define fi first
+#define sc second
 
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const ll inf = 9e15 + 7;
+const ll MOD = 1e9 + 7;
+const ll N = 1e9 + 10;
+//-----------------------------------------------------------------------------------------
+void solve(void)
+{
+    string s;
+    map<string, string> mp;
+    int ft = 0;
+    while (getline(cin, s))
+    {
+        if (s.size() == 0)
+            continue;
+        string t1, t2;
+        ll fl = 0;
+        for (auto it : s)
+        {
+            if (it == ' ')
+            {
+                fl = 1;
+                continue;
+            }
+            if (fl)
+                t2.push_back(it);
+            else
+                t1.push_back(it);
+        }
+        if (t2.size() == 0)
+        {
+            ft = 1;
+            if (mp.find(s) == mp.end())
+                cout << "eh\n";
+            else
+                cout << mp[s] << '\n';
+        }
+        else
+            mp[t2] = t1;
+    }
+}
+//-----------------------------------------------------------------------------------------
 int main()
 {
-    vector<int>arr(80, 0);
-
-    for(int i = 1; i<=75; i++){
-        for(int j = i; j<=75; j+=i){
-            arr[j]+=i;
-        }
+    // cout << fixed << showpoint << setprecision(10);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1, T;
+    // cin >> test;
+    for (T = 1; T <= test; T++)
+    {
+        // cout << "Case " << T << ": ";
+        solve();
     }
-
-    for(int i=1;i<75;i++){
-        cout << i << " : " << arr[i]<<'\n';
-    }
-
     return 0;
 }
