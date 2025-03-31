ea9874d07f05423476d48ce4f78860b91be8649f
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-03-31 20:22:58 +0600

    28-03-2025

--- Vjudge/H_Random_2.cpp
+++ Vjudge/H_Random_2.cpp
@@ -18,6 +18,46 @@ const int inf = 9e16 + 7;
 const int MOD = 1e9 + 7;
 const int N = 1e5 + 10;
 //-----------------------------------------------------------------------------------------
+inline int normal(int a, int m)
+{
+    a = a - (a / m) * m;
+    if (a < 0)
+        a += m;
+    return a;
+}
+inline int modadd(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a + b, m);
+}
+inline int modsub(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a - b, m);
+}
+inline int modmul(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a * b, m);
+}
+inline int binexpo(int a, int b, int m)
+{
+    int ans = 1;
+    a = normal(a, m);
+    while (b)
+    {
+        if (b & 1)
+            ans = modmul(ans, a, m);
+        a = modmul(a, a, m);
+        b >>= 1;
+    }
+    return normal(ans, m);
+}
+inline int moddiv(int a, int b, int m)
+{
+    return normal(modmul(a, binexpo(b, m - 2, m), m), m);
+}
+//-----------------------------------------------------------------------------------------
 void solve(void)
 {
     int n;
@@ -34,6 +74,43 @@ void solve(void)
             return;
         }
     }
+    s = '1' + s + '9';
+    int las = 1, pos = 0;
+    vector<int> v;
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] != '-')
+        {
+            int now = s[i] - '0';
+            int t = i - pos - 1;
+            if (t == 1)
+                v.push_back((now - las + 1));
+            else if (t > 0)
+            {
+                now -= (las - 1);
+                las = 1;
+                int inc = t - 1, cnt = 1;
+                // dbg(las);
+                // dbg(now);
+                // dbg(inc);
+                int tt = 0;
+                for (int i = las; i <= now; i++)
+                {
+                    tt += cnt;
+                    cnt += inc;
+                    // dbg(cnt);
+                    inc++;
+                }
+                v.push_back(tt);
+            }
+            las = s[i] - '0';
+            pos = i;
+        }
+    }
+    int ans = 1;
+    for (auto it : v)
+        ans = modmul(ans, it, MOD);
+    cout << ans << '\n';
 }
 //-----------------------------------------------------------------------------------------
 signed main()
