48e9c7028eefb5bffcc586a31b74ce874921af48
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-04-18 18:41:49 +0600

    18-04-2025

--- Codeforces/C_Number_of_Ways.cpp
+++ Codeforces/C_Number_of_Ways.cpp
@@ -18,6 +18,16 @@ const int inf = 9e16 + 7;
 const int MOD = 1e9 + 7;
 const int N = 1e5 + 10;
 //-----------------------------------------------------------------------------------------
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // multiple value
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // single
+ordered_set<ll> st; //must use ll
+// ll pos = st.order_of_key(5);
+// ll val = *st.find_by_order(2);
+//-----------------------------------------------------------------------------------------
 void solve(void)
 {
     int n, s = 0, c = 0;
