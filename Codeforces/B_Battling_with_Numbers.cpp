d53d8c3c030dd21e554ef187d0183911ad28332c
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-04-29 20:31:14 +0600

    29-04-2025

---
+++ Codeforces/B_Battling_with_Numbers.cpp
@@ -0,0 +1,123 @@
+// Author:  Rajesh Biswas
+// CF    :  rajesh-1920
+// Date  :  29.04.2025
+
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------(definition section)-----------------------------------------
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define int long long int
+#define fi first
+#define sc second
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const int inf = 9e16 + 7;
+const int MOD = 998244353;
+const int N = 1e5 + 10;
+//-----------------------------------------------------------------------------------------
+inline int normal(int a, int m)
+{
+    a = a - (a / m) * m;
+    if (a < 0)
+        a += m;
+    return a;
+}
+inline int modadd(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a + b, m);
+}
+inline int modsub(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a - b, m);
+}
+inline int modmul(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a * b, m);
+}
+inline int binexpo(int a, int b, int m)
+{
+    int ans = 1;
+    a = normal(a, m);
+    while (b)
+    {
+        if (b & 1)
+            ans = modmul(ans, a, m);
+        a = modmul(a, a, m);
+        b >>= 1;
+    }
+    return normal(ans, m);
+}
+inline int moddiv(int a, int b, int m)
+{
+    return normal(modmul(a, binexpo(b, m - 2, m), m), m);
+}
+//-----------------------------------------------------------------------------------------
+void solve(void)
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &it : v)
+        cin >> it;
+    map<int, int> x;
+    for (int i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        x[v[i]] = t;
+    }
+    int m;
+    cin >> m;
+    vector<int> vv(m);
+    for (auto &it : vv)
+        cin >> it;
+    int fl = 0;
+    for (int i = 0; i < m; i++)
+    {
+        int t;
+        cin >> t;
+        if (x[vv[i]] < t)
+            fl = 1;
+        x[vv[i]] -= t;
+    }
+    if (fl)
+    {
+        cout << 0;
+        return;
+    }
+    int ans = 1;
+    for (auto it : x)
+    {
+        if (it.sc)
+        {
+            it.sc = 1;
+            fl = 1;
+        }
+        ans = modadd(ans, it.sc, MOD);
+        // cout << it.sc << '\n';
+    }
+    if (fl)
+        ans = modmul(2, ans, MOD);
+    cout << ans << '\n';
+}
+//-----------------------------------------------------------------------------------------
+signed main()
+{
+    // cout << fixed << showpoint << setprecision(10);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1, T;
+    // cin >> test;
+    for (T = 1; T <= test; T++)
+    {
+        // cout << "Case " << T << ": ";
+        solve();
+    }
+    return 0;
+}
