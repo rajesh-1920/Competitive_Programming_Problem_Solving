4d847c5424f5022582efae4ed2f9e307698101c5
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-05-18 21:17:19 +0600

    18-05-2025

--- Codeforces/A_It_s_Time_To_Duel.cpp
+++ Codeforces/A_It_s_Time_To_Duel.cpp
@@ -1,6 +1,6 @@
 // Author:  Rajesh Biswas
 // CF    :  rajesh-1920
-// Date  :  17.05.2025
+// Date  :  18.05.2025
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,40 +16,38 @@ using namespace std;
 const double eps = 1e-1;
 const int inf = 9e16 + 7;
 const int MOD = 1e9 + 7;
-const int N = 2e5 + 10;
+const int N = 1e5 + 10;
 //-----------------------------------------------------------------------------------------
-int dp[N][2];
-int ok(int n, int fl, vector<int> &v, vector<int> &next)
-{
-    if (n == v.size())
-        return 0;
-    // dbg(n);
-    if (dp[n][fl] != -1)
-        return dp[n][fl];
-    int ans = ok(n + 1, 1, v, next);
-    ans = max(ans, ok(n + 1, 0, v, next));
-    if (next[n] != v.size())
-    {
-        if (fl)
-            ans = max(ans, ok(next[n], 0, v, next) + (next[n] - n + 1));
-        ans = max(ans, ok(next[n], fl, v, next) + (next[n] - n));
-    }
-    return dp[n][fl] = ans;
-}
 void solve(void)
 {
-    memset(dp, -1, sizeof(dp));
     int n;
     cin >> n;
-    vector<int> v(n), next(n, n), temp(n, n);
+    vector<int> v(n);
     for (auto &it : v)
         cin >> it;
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = 0; i + 1 < n; i++)
     {
-        next[i] = temp[v[i] - 1];
-        temp[v[i] - 1] = i;
+        if (v[i] == 0)
+        {
+            if (v[i + 1] == 0)
+            {
+                cout << "YES\n";
+                return;
+            }
+        }
+        else
+        {
+            if (v[i + 1] == 1)
+            {
+                if (i == 0)
+                {
+                    cout << "YES\n";
+                    return;
+                }
+            }
+        }
     }
-    cout << ok(0, 1, v, next) << '\n';
+    cout << "NO\n";
 }
 //-----------------------------------------------------------------------------------------
 signed main()
