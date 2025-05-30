08021739f85f87bcbc7a04da07fb82d5cd4a9f54
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-05-30 21:07:24 +0600

    30-05-2025

---
+++ Codeforces/C_Maximum_Subarray_Sum.cpp
@@ -0,0 +1,102 @@
+// Author:  Rajesh Biswas
+// CF    :  rajesh-1920
+// Date  :  30.05.2025
+
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------(definition section)-----------------------------------------
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define int long long int
+#define fi first
+#define sc second
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const int inf = 1e13 + 7;
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 10;
+//-----------------------------------------------------------------------------------------
+void solve(void)
+{
+    int n, k;
+    string s;
+    cin >> n >> k >> s;
+    vector<int> v(n + 2);
+    for (int i = 1; i <= n; i++)
+        cin >> v[i];
+    s = "0" + s + "0";
+    for (int i = 0; i <= n + 1; i++)
+        if (s[i] == '0')
+            v[i] = -inf;
+    int fl = 1;
+    int l = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (v[i] == -inf)
+        {
+            int lcont = 0, rcont = 0;
+            for (int j = l; j < i; j++)
+            {
+                lcont += v[j];
+                lcont = max(lcont, 0LL);
+            }
+            l = i;
+            if (lcont > k)
+                continue;
+            int r;
+            for (int j = i + 1;; j++)
+                if (v[j] == -inf)
+                {
+                    r = j;
+                    break;
+                }
+            for (int j = r; j > i; j--)
+            {
+                rcont += v[j];
+                rcont = max(rcont, 0LL);
+            }
+            if (rcont > k)
+                continue;
+            fl = 0;
+            v[i] = k - lcont - rcont;
+            break;
+        }
+    }
+    if (fl)
+    {
+        int lcont = 0;
+        for (int j = 1; j <= n; j++)
+        {
+            lcont += v[j];
+            lcont = max(lcont, 0LL);
+        }
+        if (lcont == k)
+            fl = 0;
+    }
+    if (fl)
+        cout << "No\n";
+    else
+    {
+        cout << "Yes\n";
+        for (int i = 1; i <= n; i++)
+            cout << v[i] << ' ';
+        cout << '\n';
+    }
+}
+//-----------------------------------------------------------------------------------------
+signed main()
+{
+    // cout << fixed << showpoint << setprecision(10);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1, T;
+    cin >> test;
+    for (T = 1; T <= test; T++)
+    {
+        // cout << "Case " << T << ": ";
+        solve();
+    }
+    return 0;
+}
