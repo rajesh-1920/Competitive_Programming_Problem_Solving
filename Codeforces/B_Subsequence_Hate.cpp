b9bfea5d9d13d4d8a4d4bbd9c21f2a5ff6609d81
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-07-04 21:21:56 +0600

    04-07-2025

--- Codeforces/B_Subsequence_Hate.cpp
+++ Codeforces/B_Subsequence_Hate.cpp
@@ -9,44 +9,43 @@ using namespace std;
 #define int long long int
 #define fi first
 #define sc second
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const int inf = 9e16 + 7;
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 10;
 //-----------------------------------------------------------------------------------------
 void solve(void)
 {
-    int n;
-    cin >> n;
-    vector<int> d(n);
-    for (auto &it : d)
-        cin >> it;
-    vector<pair<int, int>> vp(n);
-    for (auto &it : vp)
-        cin >> it.fi >> it.sc;
-
-    vector<int> next(n), pore_barbe(n, 0);
-    int mn = vp.back().sc;
-    for (int i = n - 1; i >= 0; i--)
+    string s;
+    cin >> s;
+    if (s.size() < 3)
     {
-        next[i] = mn = min(mn, vp[i].sc);
-        if (i + 1 < n)
-            pore_barbe[i] = pore_barbe[i + 1];
-        pore_barbe[i] += (d[i] == 1);
+        cout << 0 << '\n';
+        return;
     }
-
-    int curr = 0;
-    for (int i = 0; i < n; i++)
+    int ans = s.size();
+    vector<int> c0(ans, 0), c1(ans, 0);
+    for (int i = 0; i < s.size(); i++)
     {
-        if (d[i] == -1)
-            d[i] = (curr + 1 + pore_barbe[i] <= next[i]);
-        if (d[i] == 1)
-            curr++;
-        if (!(vp[i].fi <= curr && curr <= vp[i].sc))
-        {
-            cout << -1 << '\n';
-            return;
-        }
+        if (i)
+            c0[i] = c0[i - 1], c1[i] = c1[i - 1];
+        if (s[i] == '0')
+            c0[i]++;
+        else
+            c1[i]++;
     }
-    for (auto it : d)
-        cout << it << ' ';
-    cout << '\n';
+    for (int i = 0; i < s.size(); i++)
+    {
+        int t = c0[i] + (c1[s.size() - 1] - c1[i]);
+        ans = min(ans, t);
+        t = c1[i] + (c0[s.size() - 1] - c0[i]);
+        ans = min(ans, t);
+    }
+    cout << ans << '\n';
 }
 //-----------------------------------------------------------------------------------------
 signed main()
