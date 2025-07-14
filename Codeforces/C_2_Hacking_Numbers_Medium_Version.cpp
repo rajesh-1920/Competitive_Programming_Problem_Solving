149ecf8d5ebf64f2760058f333fa1d7e5333e0dc
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-07-15 00:46:12 +0600

    15-07-2025

---
+++ Codeforces/C_2_Hacking_Numbers_Medium_Version.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    int x;
+    cout << "mul 9" << '\n';
+    cin >> x;
+    cout << "digit\n";
+    cin >> x;
+    cout << "digit\n";
+    cin >> x;
+    cout << "add " << n - 9 << '\n';
+    cin >> x;
+    cout << "!\n";
+    cin >> x;
+}
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
