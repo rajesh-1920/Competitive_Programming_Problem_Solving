e9449982fd9280944c8eb42805f2d6d066d86432
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-07-16 21:45:00 +0600

    16-07-2025

--- Codeforces/C_Racing.cpp
+++ Codeforces/C_Racing.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #define dbg(x) cout << #x << " = " << x << '\n'
 #define fi first
 #define sc second
+#define int long long int
 //-----------------------------------------------------------------------------------------
 void solve(void)
 {
@@ -19,47 +20,43 @@ void solve(void)
     vector<pair<int, int>> vp(n);
     for (auto &it : vp)
         cin >> it.fi >> it.sc;
-
-    vector<int> next(n), pore_barbe(n, 0);
-    int mn = vp.back().sc;
-    for (int i = n - 1; i >= 0; i--)
-    {
-        if (i)
-            next[i - 1] = mn = min(mn, vp[i].sc);
-        if (i + 1 < n)
-            pore_barbe[i] = pore_barbe[i + 1];
-        pore_barbe[i] += (d[i] == 1);
-    }
-
+    vector<int> next(n), hate;
     int curr = 0;
-    for (int i = 0; i + 1 < n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (d[i] == -1)
-            d[i] = (curr + 1 + pore_barbe[i] <= next[i] && curr + 1 <= vp[i].sc);
-        if (d[i] == 1)
+            hate.push_back(i);
+        else
+            curr += d[i];
+        while (curr < vp[i].fi)
+        {
+            if (hate.empty())
+            {
+                cout << -1 << '\n';
+                return;
+            }
+            d[hate.back()] = 1;
             curr++;
-        if (!(vp[i].fi <= curr && curr <= vp[i].sc))
+            hate.pop_back();
+        }
+        if (curr > vp[i].sc or curr < vp[i].fi)
         {
             cout << -1 << '\n';
             return;
         }
     }
-    if (d[n - 1] == -1)
+    curr = 0;
+    for (int i = 0; i < n; i++)
     {
-        if (curr < vp[n - 1].sc)
-            d[n - 1] = 1, curr++;
-        else
-            d[n - 1] = 0;
-    }
-    else if (d[n - 1] == 1)
-        curr++;
-    if (!(vp[n - 1].fi <= curr && curr <= vp[n - 1].sc))
-    {
-        cout << -1 << '\n';
-        return;
+        curr += d[i] == 1;
+        if (curr > vp[i].sc or curr < vp[i].fi)
+        {
+            cout << -1 << '\n';
+            return;
+        }
     }
     for (auto it : d)
-        cout << it << ' ';
+        cout << max(0ll, it) << ' ';
     cout << '\n';
 }
 //-----------------------------------------------------------------------------------------
