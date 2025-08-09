173320c6df19eb20c336f722df746b0737d5ca28
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-08-09 22:11:25 +0600

    09-08-2025

--- Codeforces/B_The_Number_of_Products.cpp
+++ Codeforces/B_The_Number_of_Products.cpp
@@ -22,8 +22,44 @@ void solve(void)
 {
     int n;
     cin >> n;
-    // vector<int> v(n); for (auto &it : v) cin >> it;
-    // vector<vector<int>> v(110, vector<int>(110, 0));
+    vector<int> v(n), neg(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        if (i)
+            neg[i] = neg[i - 1];
+        if (v[i] < 0)
+            neg[i]++;
+    }
+    int anev = 0, ev = 0, od = 0, odd = 0, evv = 0, anod = 0;
+    for (int i = 0; i < n; i++)
+        if (neg[i] & 1)
+            od++, odd++;
+        else
+            ev++, evv++;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] < 0)
+        {
+            if (neg[i] & 1)
+                anev += ev, anod += od;
+            else
+                anev += od, anod += ev;
+        }
+        else
+        {
+            if (neg[i] & 1)
+                anev += od, anod += ev;
+            else
+                anev += ev, anod += od;
+        }
+
+        if (neg[i] & 1)
+            od--, odd--;
+        else
+            ev--, evv--;
+    }
+    cout << anod << ' ' << anev << '\n';
 }
 //-----------------------------------------------------------------------------------------
 signed main()
@@ -32,7 +68,7 @@ signed main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int test = 1, T;
-    cin >> test;
+    // cin >> test;
     for (T = 1; T <= test; T++)
     {
         // cout << "Case " << T << ": ";
