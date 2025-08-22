d2f8b4b87a1e1b3cf300b857cd53cc0a0417894e
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-08-22 21:04:59 +0600

    22-08-2025

---
+++ Codeforces/E_Queen_of_Diamonds.cpp
@@ -0,0 +1,122 @@
+// Author:  Rajesh Biswas
+// CF    :  rajesh-1920
+// Date  :  21.08.2025
+
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------(definition section)-----------------------------------------
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define int long long int
+#define fi first
+#define sc second
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const int inf = 9e16 + 7;
+const int MOD = 1e9 + 7;
+const int N = 2e6 + 10;
+//------------------------------(solve)----------------------------------------------------
+inline int normal(int a, int m)
+{
+    a = a - (a / m) * m;
+    if (a < 0)
+        a += m;
+    return a;
+}
+inline int modadd(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a + b, m);
+}
+inline int modsub(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a - b, m);
+}
+inline int modmul(int a, int b, int m)
+{
+    a = normal(a, m), b = normal(b, m);
+    return normal(a * b, m);
+}
+inline int binexpo(int a, int b, int m)
+{
+    int ans = 1;
+    a = normal(a, m);
+    while (b)
+    {
+        if (b & 1)
+            ans = modmul(ans, a, m);
+        a = modmul(a, a, m);
+        b >>= 1;
+    }
+    return normal(ans, m);
+}
+inline int moddiv(int a, int b, int m)
+{
+    return normal(modmul(a, binexpo(b, m - 2, m), m), m);
+}
+//-----------------------------------------------------------------------------------------
+vector<int> fact(N);
+void ff()
+{
+    fact[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fact[i] = modmul(i, fact[i - 1], MOD);
+    }
+}
+
+void solve(void)
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    map<char, int> mp;
+    for (auto it : s)
+        mp[it]++;
+    int ans = 1;
+    for (auto it : mp)
+    {
+        int t = it.sc, sum = 0;
+        //dbg(t);
+        if (t & 1)
+        {
+            for (int i = 1; i <= t; i += 2)
+            {
+                int d = modmul(fact[i], fact[t - i], MOD);
+                d = moddiv(fact[t], d, MOD);
+                sum = modadd(sum, d, MOD);
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= t; i += 2)
+            {
+                int d = modmul(fact[i], fact[t - i], MOD);
+                d = moddiv(fact[t], d, MOD);
+                sum = modadd(sum, d, MOD);
+            }
+        }
+        //dbg(sum);
+        ans = modmul(ans, sum, MOD);
+    }
+    cout << ans << '\n';
+}
+//-----------------------------------------------------------------------------------------
+signed main()
+{
+    // cout << fixed << showpoint << setprecision(10);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1, T;
+    cin >> test;
+    ff();
+    for (T = 1; T <= test; T++)
+    {
+        // cout << "Case " << T << ": ";
+        solve();
+    }
+    return 0;
+}
