d40b6a56596f5692f67aa24c20fabf834f4ed9c4
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-08-24 19:50:07 +0600

    24-08-2025

--- Codeforces/G_GCD_vs_LCM.cpp
+++ Codeforces/G_GCD_vs_LCM.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 //----------------------------(definition section)-----------------------------------------
 #define dbg(x) cout << #x << " = " << x << '\n'
-#define int long long int
+#define int unsigned long long int
 #define fi first
 #define sc second
 
@@ -16,14 +16,43 @@ using namespace std;
 const double eps = 1e-1;
 const int inf = 9e16 + 7;
 const int MOD = 1e9 + 7;
-const int N = 1e5 + 10;
+const int N = 100 + 10;
 //------------------------------(solve)----------------------------------------------------
+vector<int> pp;
+void ss()
+{
+    pp.push_back(2);
+    for (int i = 3; i < N; i += 2)
+    {
+        int fl = 1;
+        for (int j = 2; j < i; j++)
+            if (i % j == 0)
+            {
+                fl = 0;
+                break;
+            }
+        if (fl)
+            pp.push_back(i);
+    }
+    sort(all(pp));
+}
 void solve(void)
 {
-    int n;
-    cin >> n;
-    // vector<int> v(n); for (auto &it : v) cin >> it;
-    // vector<vector<int>> v(110, vector<int>(110, 0));
+    int n, x;
+    cin >> n >> x;
+    int t = 1;
+    for (int i = 0; i < n; i++)
+    {
+        t *= pp[i];
+        if (t > x)
+        {
+            cout << -1 << '\n';
+            return;
+        }
+    }
+    for (int i = 0; i < n; i++)
+        cout << t / pp[i] << ' ';
+    cout << '\n';
 }
 //-----------------------------------------------------------------------------------------
 signed main()
@@ -33,6 +62,7 @@ signed main()
     cin.tie(NULL);
     int test = 1, T;
     cin >> test;
+    ss();
     for (T = 1; T <= test; T++)
     {
         // cout << "Case " << T << ": ";
