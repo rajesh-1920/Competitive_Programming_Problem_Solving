f18e42eacc1cf8bae521f284d35fc56a1f4810f0
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-10-22 19:31:20 +0600

    Come once again

--- Codeforces/B_Meeting_on_the_Line.cpp
+++ Codeforces/B_Meeting_on_the_Line.cpp
@@ -13,40 +13,41 @@ using namespace std;
 #define all(s) s.begin(), s.end()
 #define rall(s) s.rbegin(), s.rend()
 
-const double eps = 1e-1;
+const double eps = 1e-7;
 const int inf = 9e16 + 7;
 const int MOD = 1e9 + 7;
 const int N = 1e5 + 10;
 //------------------------------(solve)----------------------------------------------------
 void solve(void)
 {
-    int n;
+    int n, cnt = 200;
     cin >> n;
-    vector<int> g[n + 1];
-    set<pair<int, int>> st;
-    for (int i = 0; i < n; i++)
+    vector<double> v(n), t(n);
+    for (auto &it : v)
+        cin >> it;
+    for (auto &it : t)
+        cin >> it;
+    double l = 0, r = MOD;
+    while (cnt--)
     {
-        int x, y;
-        cin >> x >> y;
-        g[x].push_back(y);
-        //g[y].push_back(x);
-        st.insert({x, y});
-        st.insert({y, x});
+        double m1 = l + (r - l) / 3, m2 = r - (r - l) / 3;
+        double t1 = 0, t2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            t1 = max(t1, (t[i] + max(v[i] - m1, m1 - v[i])));
+            t2 = max(t2, (t[i] + max(v[i] - m2, m2 - v[i])));
+        }
+        if (t1 < t2)
+            r = m2;
+        else
+            l = m1;
     }
-    for (int i = 1; i <= n; i++)
-        for (auto it : g[i])
-            for (auto ii : g[i])
-                if (st.find({it, ii}) != st.end())
-                {
-                    cout << "NO\n";
-                    return;
-                }
-    cout << "YES\n";
+    cout << (l + r) / 2.0 << '\n';
 }
 //-----------------------------------------------------------------------------------------
 signed main()
 {
-    // cout << fixed << showpoint << setprecision(10);
+    cout << fixed << showpoint << setprecision(7);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int test = 1, T;
