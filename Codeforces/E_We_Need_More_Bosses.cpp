b363d5a46e0f3fcfd815935929d7261203a34e23
rajesh-1920 <rajesh18@cse.pstu.ac.bd>
2026-02-21 19:59:16 +0600

    21-02-2026

--- Codeforces/E_We_Need_More_Bosses.cpp
+++ Codeforces/E_We_Need_More_Bosses.cpp
@@ -16,16 +16,14 @@ using namespace std;
 const double eps = 1e-1;
 const int inf = 9e16 + 7;
 const int MOD = 1e9 + 7;
-
+//----------------------------(solve section)-----------------------------------------------------
 void dfs(int n, int par, vector<int> &vis, vector<int> &intime, vector<int> &low, int &cnt, set<int> &ans, vector<vector<pair<int, int>>> &v)
 {
-    vis[n] = 1;
-    intime[n] = low[n] = cnt++;
+    vis[n] = 1, intime[n] = low[n] = cnt++;
     for (auto &it : v[n])
-    {
         if (it.fi == par)
             continue;
-        if (vis[it.fi])
+        else if (vis[it.fi])
             low[n] = min(low[n], intime[it.fi]);
         else
         {
@@ -34,13 +32,10 @@ void dfs(int n, int par, vector<int> &vis, vector<int> &intime, vector<int> &low
                 ans.insert(it.sc);
             low[n] = min(low[n], low[it.fi]);
         }
-    }
 }
-
 void merge(int n, int &pr, vector<int> &vis, vector<int> &par, vector<vector<int>> &g)
 {
-    vis[n] = 1;
-    par[n] = pr;
+    vis[n] = 1, par[n] = pr;
     for (auto &it : g[n])
         if (!vis[it])
             merge(it, pr, vis, par, g);
@@ -48,16 +43,15 @@ void merge(int n, int &pr, vector<int> &vis, vector<int> &par, vector<vector<int
 void diameter(int n, int pr, vector<int> &depth, vector<vector<int>> &g)
 {
     for (auto &it : g[n])
-    {
-        if (it == pr)
-            continue;
-        depth[it] = depth[n] + 1;
-        diameter(it, n, depth, g);
-    }
+        if (it != pr)
+        {
+            depth[it] = depth[n] + 1;
+            diameter(it, n, depth, g);
+        }
 }
 void solve()
 {
-    int n, m;
+    int n, m, cnt = 0;
     cin >> n >> m;
     vector<pair<int, int>> edge;
     vector<vector<pair<int, int>>> v(n + 1);
@@ -67,37 +61,35 @@ void solve()
         v[x].push_back({y, edge.size()}), v[y].push_back({x, edge.size()});
         edge.push_back({x, y});
     }
-    vector<int> vis(n + 5, 0), intime(n + 5, 0), low(n + 5, 0);
-    int cnt = 0;
+    vector<int> vis(n + 5, 0), intime(n + 5, 0), low(n + 5, 0), par(n + 5, -1), depth(n + 5, 0);
     set<int> st;
     dfs(1, 0, vis, intime, low, cnt, st, v);
+
     vector<vector<int>> g(n + 1), dia(n + 1);
     for (int i = 0; i < m; i++)
-    {
-        if (st.find(i) != st.end())
-            continue;
-        g[edge[i].fi].push_back(edge[i].sc), g[edge[i].sc].push_back(edge[i].fi);
-    }
-    vector<int> par(n + 5, -1), vvis(n + 5, 0);
+        if (st.find(i) == st.end())
+            g[edge[i].fi].push_back(edge[i].sc), g[edge[i].sc].push_back(edge[i].fi);
+    vis.assign(n + 5, 0LL);
     for (int i = 1; i <= n; i++)
-        if (!vvis[i])
-            merge(i, i, vvis, par, g);
+        if (!vis[i])
+            merge(i, i, vis, par, g);
+
     for (auto &it : st)
         dia[par[edge[it].fi]].push_back(par[edge[it].sc]), dia[par[edge[it].sc]].push_back(par[edge[it].fi]);
-    vector<int> depth(n + 5, 0), depth1(n + 5, 0);
     diameter(1, 0, depth, dia);
-    int mx = 0, pos = 1;
+    cnt = 0, m = 1;
     for (int i = 1; i <= n; i++)
-        if (depth[i] > mx)
-            mx = depth[i], pos = i;
-    diameter(pos, 0, depth1, dia);
+    {
+        if (depth[i] > cnt)
+            cnt = depth[i], m = i;
+        depth[i] = 0;
+    }
+    diameter(m, 0, depth, dia);
     for (int i = 1; i <= n; i++)
-        mx = max(mx, depth1[i]);
-    cout << mx << '\n';
+        cnt = max(cnt, depth[i]);
+    cout << cnt << '\n';
 }
-
 //-----------------------------(Suduko)----------------------------------------------------
-
 signed main()
 {
     // cout << fixed << showpoint << setprecision(10);
